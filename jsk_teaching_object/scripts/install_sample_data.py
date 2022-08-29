100342b3e793e60483e37094584fd2b6ef9063e1
iory <ab.ioryz@gmail.com>
2022-08-29 13:55:26 +0900

    [jsk_teaching_object] Add sample

--- jsk_teaching_object/scripts/install_sample_data.py
+++ jsk_teaching_object/scripts/install_sample_data.py
@@ -14,6 +14,14 @@ def main():
         extract=False,
     )
 
+    download_data(
+        pkg_name=PKG,
+        path='sample/data/2022-05-18-19-02-34-daily-object.bag',
+        url='https://drive.google.com/uc?id=1n2eJQuoxC7h488pjd7UMZ_zRXk8n7Iif',
+        md5='74f3292c4a53a2725e1ba359ec01cde4',
+        extract=False,
+    )
+
 
 if __name__ == '__main__':
     main()
