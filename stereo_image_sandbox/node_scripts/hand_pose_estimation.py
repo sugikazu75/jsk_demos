1612cc61c5e4b4f229b0cea20a9af8bcbcb73a30
iory <ab.ioryz@gmail.com>
2022-09-12 14:03:39 +0900

    Fixed flip

--- stereo_image_sandbox/node_scripts/hand_pose_estimation.py
+++ stereo_image_sandbox/node_scripts/hand_pose_estimation.py
@@ -108,9 +108,7 @@ class HandPoseEstimation(ConnectionBasedTransport):
 
         image = bridge.imgmsg_to_cv2(img_msg, desired_encoding='bgr8')
 
-        # Flip the image horizontally for a later selfie-view display, and convert
-        # the BGR image to RGB.
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         image.flags.writeable = False
