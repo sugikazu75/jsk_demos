959e60495569eda65e648517fc4461a2a973e2d2
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2022-10-18 22:04:18 +0900

    add camera transformer

---
+++ kxr_flight_unit_recognition/scripts/transformer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped
+from apriltag_ros.msg import AprilTagDetectionArray
+
+class CameraTransformer:
+    def __init__(self):
+        self.mocap_pose_sub = rospy.Subscriber("/mocap/pose", PoseStamped, self.mocapCallback)
+
+        self.br = tf.TransformBroadcaster()
+
+    def mocapCallback(self, msg):
+        self.br.sendTransform((msg.pose.position.x, msg.pose.position.y, msg.pose.position.z), (msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w), rospy.Time.now(), 'flight_unit', 'world')
+
+
+if __name__ == "__main__":
+    rospy.init_node("transformer_node")
+    node = CameraTransformer()
+    rospy.spin()
