55197fd118df53ca1bc880e283cf5225d0ed8136
sugihara <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2023-01-06 20:09:21 +0900

    [bin] update rosbag record script

--- kxr_flight_unit_recognition/bin/rosbag_apriltag_detection.sh
+++ kxr_flight_unit_recognition/bin/rosbag_apriltag_detection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "record apriltag detection data"
-rosrun kxr_flight_unit_control rosbag_birotor_all_data.sh /stereo/image_raw/compressed /stereo/left_camera/image_raw/compressed /stereo/left_camera/image_rect_color/compressed /stereo/left_camera/tag_detections /stereo/left_camera/tag_detections_image/compressed /stereo/right_camera/image_raw/compressed /stereo/right_camera/image_rect_color/compressed /stereo/right_camera/tag_detections /stereo/right_camera/tag_detections_image/compressed ${@:1}
+rosrun kxr_flight_unit_control rosbag_image_data.sh /stereo/left_camera/tag_detections /stereo/left_camera/tag_detections_image/compressed /stereo/right_camera/image_raw/compressed /stereo/right_camera/image_rect_color/compressed /stereo/right_camera/tag_detections /stereo/right_camera/tag_detections_image/compressed ${@:1}
